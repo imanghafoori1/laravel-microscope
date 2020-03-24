f75754b0e4d5f2215b91ce8ec31755028932bb9e
imanghafoori <iman_github1@yahoo.com>
2020-03-24 05:14:14 +0430

    added feature to check for route definitions

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,17 +2,10 @@
 
 namespace Imanghafoori\LaravelSelfTest\Tests;
 
-use Orchestra\Testbench\TestCase;
-use Imanghafoori\LaravelSelfTest\LaravelSelfTestServiceProvider;
+use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-
-    protected function getPackageProviders($app)
-    {
-        return [LaravelSelfTestServiceProvider::class];
-    }
-
     /** @test */
     public function true_is_true()
     {
