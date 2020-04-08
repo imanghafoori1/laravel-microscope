35959d9aac1c8e226cc03f817f307169cc3be1fb
Iman <imanghafoori1@gmail.com>
2020-04-08 01:19:37 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/CheckImports.php
+++ src/CheckImports.php
@@ -5,12 +5,10 @@ namespace Imanghafoori\LaravelMicroscope\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Model;
 use Imanghafoori\LaravelMicroscope\CheckClasses;
+use Imanghafoori\LaravelMicroscope\Checks\CheckClassReferences;
 use Imanghafoori\LaravelMicroscope\CheckViewRoute;
 use Imanghafoori\LaravelMicroscope\ErrorPrinter;
 use Imanghafoori\LaravelMicroscope\Util;
-use Imanghafoori\LaravelMicroscope\Checks\CheckRouteCalls;
-use Imanghafoori\LaravelMicroscope\Checks\CheckClassReferences;
-use Imanghafoori\LaravelMicroscope\Checks\CheckViewFilesExistence;
 
 class CheckImports extends Command
 {
