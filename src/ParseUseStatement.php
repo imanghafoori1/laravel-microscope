6a6087cd4d12c3f62e89fd2cba004aeb59768b5d
Iman <imanghafoori1@gmail.com>
2020-04-09 08:33:56 +0430

    bug fix for new self

--- src/ParseUseStatement.php
+++ src/ParseUseStatement.php
@@ -77,6 +77,9 @@ class ParseUseStatement
             'array',
             'callable',
             '::',
+            'self',
+            'static',
+            'parent',
         ], true);
     }
 
