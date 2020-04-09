db0fe98050986b0b2dead19cd33efa913021fecb
Iman <imanghafoori1@gmail.com>
2020-04-09 02:30:11 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/ParseUseStatement.php
+++ src/ParseUseStatement.php
@@ -230,38 +230,38 @@ class ParseUseStatement
             $results = [];
             $namespace = '';
             foreach ($classes as $i => $rows) {
-                    if ($rows[0][0] == T_NAMESPACE) {
-                        unset($rows[0]);
-                        foreach ($rows as $row) {
-                            $namespace .= $row[1];
-                        }
+                if ($rows[0][0] == T_NAMESPACE) {
+                    unset($rows[0]);
+                    foreach ($rows as $row) {
+                        $namespace .= $row[1];
+                    }
+                    continue;
+                }
+
+                $results[$i]['class'] = '';
+
+                // attach the current namespace if it does not begin with '\'
+                if ($rows[0][1] != '\\') {
+                    $results[$i]['class'] = $namespace ? $namespace.'\\' : '';
+                }
+
+                foreach ($rows as $row) {
+                    if (self::isBuiltinType($row[1])) {
+                        unset($results[$i]);
                         continue;
                     }
-
-                    $results[$i]['class'] = '';
-
-                    // attach the current namespace if it does not begin with '\'
                     if ($rows[0][1] != '\\') {
-                        $results[$i]['class'] = $namespace ? $namespace.'\\' : '';
-                    }
-
-                    foreach ($rows as $row) {
-                        if (self::isBuiltinType($row[1])) {
-                            unset($results[$i]);
-                            continue;
-                        }
-                        if ($rows[0][1] != '\\') {
-                            if (isset(array_values($imports)[0][$rows[0][1]][0])) {
-                                $results[$i]['class'] = array_values($imports)[0][$rows[0][1]][0];
-                            } else {
-                                $results[$i]['class'] .= $row[1];
-                            }
+                        if (isset(array_values($imports)[0][$rows[0][1]][0])) {
+                            $results[$i]['class'] = array_values($imports)[0][$rows[0][1]][0];
                         } else {
                             $results[$i]['class'] .= $row[1];
                         }
-                        $results[$i]['line'] = $row[2];
+                    } else {
+                        $results[$i]['class'] .= $row[1];
                     }
+                    $results[$i]['line'] = $row[2];
                 }
+            }
 
             return $results;
         } catch (\ErrorException $e) {
