35dfbaf0317ccffa34b13b2c280774f6a0c5d449
innoflash <innocentmazando@gmail.com>
2020-04-10 19:09:25 +0200

    added checking imports

--- src/Commands/CheckImports.php
+++ src/Commands/CheckImports.php
@@ -7,19 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 use Imanghafoori\LaravelMicroscope\CheckClasses;
 use Imanghafoori\LaravelMicroscope\Checks\CheckClassReferences;
 use Imanghafoori\LaravelMicroscope\CheckViewRoute;
+use Imanghafoori\LaravelMicroscope\Contracts\FileCheckContract;
 use Imanghafoori\LaravelMicroscope\ErrorPrinter;
 use Imanghafoori\LaravelMicroscope\Traits\LogsErrors;
+use Imanghafoori\LaravelMicroscope\Traits\ScansFiles;
 use Imanghafoori\LaravelMicroscope\Util;
 
-class CheckImports extends Command
+class CheckImports extends Command implements FileCheckContract
 {
     use LogsErrors;
+    use ScansFiles;
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'check:imports';
+    protected $signature = 'check:imports {--d|detailed : Show files being checked}';
 
     /**
      * The console command description.
@@ -45,7 +49,7 @@ class CheckImports extends Command
 
         foreach ($psr4 as $psr4Namespace => $psr4Path) {
             $files = CheckClasses::getAllPhpFiles($psr4Path);
-            CheckClasses::checkImports($files, base_path(), $psr4Path, $psr4Namespace);
+            CheckClasses::checkImports($files, base_path(), $psr4Path, $psr4Namespace, $this);
         }
 
         (new CheckViewRoute)->check([
