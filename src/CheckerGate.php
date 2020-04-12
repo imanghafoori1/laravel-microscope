8f9b5b1163a4a31ff28c93f4b429d39333bc6d61
innoflash <innocentmazando@gmail.com>
2020-04-12 22:41:47 +0200

    fixed CheckerGate

--- src/CheckerGate.php
+++ src/CheckerGate.php
@@ -20,26 +20,26 @@ class CheckerGate extends Gate
         try {
             $policy = app()->make($class);
         } catch (BindingResolutionException $e) {
-            return app(ErrorPrinter::class)->print("The $callback callback for Gate, does not refer to a resolvable class, for ability: $ability");
+            return app(ErrorPrinter::class)->others("The $callback callback for Gate, does not refer to a resolvable class, for ability: $ability");
         }
 
         if (! method_exists($policy, $method)) {
-            return app(ErrorPrinter::class)->print("The $callback callback for Gate, does not refer to a valid method, for ability: $ability");
+            return app(ErrorPrinter::class)->others("The $callback callback for Gate, does not refer to a valid method, for ability: $ability");
         }
     }
 
     public function policy($model, $policy)
     {
         if (! class_exists($model)) {
-            return app(ErrorPrinter::class)->print("The \"$model\" you are trying to define policy for, does not exist as a valid eloquent class.");
+            return app(ErrorPrinter::class)->others("The \"$model\" you are trying to define policy for, does not exist as a valid eloquent class.");
         }
 
         if (! is_subclass_of($model, Model::class)) {
-            return app(ErrorPrinter::class)->print("The \"$model\" you are trying to define policy for, is not an eloquent model class.");
+            return app(ErrorPrinter::class)->others("The \"$model\" you are trying to define policy for, is not an eloquent model class.");
         }
 
         if (! class_exists($policy)) {
-            return app(ErrorPrinter::class)->print("The \"$policy\" is not a valid class to be used as policy for $model.");
+            return app(ErrorPrinter::class)->others("The \"$policy\" is not a valid class to be used as policy for $model.");
         }
     }
 }
