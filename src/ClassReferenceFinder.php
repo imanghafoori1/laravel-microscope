be9fabaa9161398433c21b164a97b118a59fdf23
Iman <imanghafoori1@gmail.com>
2020-04-14 11:32:10 +0430

    bug fix

--- src/ClassReferenceFinder.php
+++ src/ClassReferenceFinder.php
@@ -4,6 +4,75 @@ namespace Imanghafoori\LaravelMicroscope;
 
 class ClassReferenceFinder
 {
+
+    /**
+     * @param  string  $type
+     *
+     * @return bool
+     */
+    public static function isBuiltinType($type)
+    {
+        return in_array(strtolower($type), [
+            'string',
+            'int',
+            'float',
+            'bool',
+            'array',
+            'callable',
+            '::',
+            'self',
+            'static',
+            'parent',
+        ], true);
+    }
+    /**
+     * @param  array  $classes
+     * @param  array  $imports
+     *
+     * @return array
+     */
+    static function expendReferences($classes, $imports)
+    {
+        // Here we implode the tokens to form the full namespaced class path
+        $results = [];
+        $namespace = '';
+        foreach ($classes as $i => $rows) {
+            if ($rows[0][0] == T_NAMESPACE) {
+                unset($rows[0]);
+                foreach ($rows as $row) {
+                    $namespace .= $row[1];
+                }
+                continue;
+            }
+
+            $results[$i]['class'] = '';
+
+            // attach the current namespace if it does not begin with '\'
+            if ($rows[0][1] != '\\') {
+                $results[$i]['class'] = $namespace ? $namespace.'\\' : '';
+            }
+
+            foreach ($rows as $row) {
+                if (self::isBuiltinType($row[1])) {
+                    unset($results[$i]);
+                    continue;
+                }
+                if ($rows[0][1] != '\\') {
+                    if (isset(array_values($imports)[0][$rows[0][1]][0])) {
+                        $results[$i]['class'] = array_values($imports)[0][$rows[0][1]][0];
+                    } else {
+                        $results[$i]['class'] .= $row[1];
+                    }
+                } else {
+                    $results[$i]['class'] .= $row[1];
+                }
+                $results[$i]['line'] = $row[2];
+            }
+        }
+
+        return $results;
+    }
+
     /**
      * @param  array  $tokens
      *
@@ -42,7 +111,7 @@ class ClassReferenceFinder
             } elseif ($t == T_NAMESPACE) {
                 $force_close = false;
                 $collect = true;
-            // continue;   // why we do not continue?? (0_o)
+                // continue;   // why we do not continue?? (0_o)
             } elseif ($t == T_FUNCTION) {
                 $isDefiningFunction = true;
                 if ($isInSideClass and ! $isInsideMethod) {
--- src/ParseUseStatement.php
+++ src/ParseUseStatement.php
@@ -9,59 +9,6 @@ class ParseUseStatement
     /** @var array */
     private static $cache = [];
 
-    /**
-     * Expands class name into full name.
-     *
-     * @param  string
-     *
-     * @param  ReflectionClass  $rc
-     *
-     * @return string  full name
-     */
-    public static function expandClassName($name, ReflectionClass $rc)
-    {
-        $lower = strtolower($name);
-        if (empty($name)) {
-            throw new \InvalidArgumentException('Class name must not be empty.');
-        } elseif (self::isBuiltinType($lower)) {
-            return $lower;
-        } elseif ($lower === 'self' || $lower === 'static' || $lower === '$this') {
-            return $rc->getName();
-        } elseif ($name[0] === '\\') { // fully qualified name
-            return ltrim($name, '\\');
-        }
-        $uses = self::getUseStatements($rc);
-        $parts = explode('\\', $name, 2);
-        if (isset($uses[$parts[0]])) {
-            $parts[0] = $uses[$parts[0]];
-
-            return $parts[0][0];
-        } elseif ($rc->inNamespace()) {
-            return $rc->getNamespaceName().'\\'.$name;
-        } else {
-            return $name;
-        }
-    }
-
-    /**
-     * @param  \ReflectionClass  $class
-     *
-     * @return array of [alias => class]
-     */
-    public static function getUseStatements(ReflectionClass $class)
-    {
-        if (! isset(self::$cache[$name = $class->getName()])) {
-            if ($class->isInternal()) {
-                self::$cache[$name] = [];
-            } else {
-                $code = file_get_contents($class->getFileName());
-                self::$cache = self::parseUseStatements(token_get_all($code), $name)[0] + self::$cache;
-            }
-        }
-
-        return self::$cache[$name];
-    }
-
     public static function getUseStatementsByPath($namespacedClassName, $absPath = null)
     {
         if (! isset(self::$cache[$namespacedClassName])) {
@@ -73,27 +20,6 @@ class ParseUseStatement
         return self::$cache[$namespacedClassName];
     }
 
-    /**
-     * @param  string  $type
-     *
-     * @return bool
-     */
-    public static function isBuiltinType($type)
-    {
-        return in_array(strtolower($type), [
-            'string',
-            'int',
-            'float',
-            'bool',
-            'array',
-            'callable',
-            '::',
-            'self',
-            'static',
-            'parent',
-        ], true);
-    }
-
     public static function findClassReferences(&$tokens, $absFilePath)
     {
         try {
@@ -101,73 +27,12 @@ class ParseUseStatement
             $imports = $imports[0] ?: [$imports[1]];
             $classes = ClassReferenceFinder::process($tokens);
 
-            return self::expendClassReferences($classes, $imports);
+            return ClassReferenceFinder::expendReferences($classes, $imports);
         } catch (\ErrorException $e) {
-            dump('=====================================');
-            dump('was not able to properly parse the: '.$absFilePath.' file.');
-            dump('Please open up an issue on the github repo');
-            dump('https://github.com/imanghafoori1/laravel-microscope/issues');
-            dump('and also send the content of the file to the maintainer to fix the issue.');
-            dump('=============== Thanks ===============');
-            sleep(3);
+            self::requestIssue($absFilePath);
 
             return [];
         }
-    }
-
-    public static function findUseStatements(&$tokens)
-    {
-        $class = $classLevel = $level = null;
-        $uses = [];
-        while ($token = current($tokens)) {
-            next($tokens);
-            switch (is_array($token) ? $token[0] : $token) {
-                case T_USE:
-                    while (! $class && ($name = self::fetch($tokens, [
-                        T_STRING,
-                        T_NS_SEPARATOR,
-                    ]))) {
-                        $name = ltrim($name, '\\');
-                        if (self::fetch($tokens, '{')) {
-                            while ($suffix = self::fetch($tokens, [
-                                T_STRING,
-                                T_NS_SEPARATOR,
-                            ])) {
-                                if (self::fetch($tokens, T_AS)) {
-                                    $uses[self::fetch($tokens, T_STRING)] = [$name.$suffix, $token[2]];
-                                } else {
-                                    $tmp = explode('\\', $suffix);
-                                    $uses[end($tmp)] = [$name.$suffix, $token[2]];
-                                }
-                                if (! self::fetch($tokens, ',')) {
-                                    break;
-                                }
-                            }
-                        } elseif (self::fetch($tokens, T_AS)) {
-                            $uses[self::fetch($tokens, T_STRING)] = [$name, $token[2]];
-                        } else {
-                            $tmp = explode('\\', $name);
-                            $uses[end($tmp)] = [$name, $token[2]];
-                        }
-                        if (! self::fetch($tokens, ',')) {
-                            break;
-                        }
-                    }
-                    break;
-                case T_CURLY_OPEN:
-                case T_DOLLAR_OPEN_CURLY_BRACES:
-                case '{':
-                    $level++;
-                    break;
-                case '}':
-                    if ($level === $classLevel) {
-                        $class = $classLevel = null;
-                    }
-                    $level--;
-            }
-        }
-
-        return $uses;
     }
 
     /**
@@ -283,50 +148,16 @@ class ParseUseStatement
     }
 
     /**
-     * @param  array  $classes
-     * @param  array  $imports
-     *
-     * @return array
+     * @param $absFilePath
      */
-    protected static function expendClassReferences($classes, array $imports)
+    protected static function requestIssue($absFilePath): void
     {
-        // Here we implode the tokens to form the full namespaced class path
-        $results = [];
-        $namespace = '';
-        foreach ($classes as $i => $rows) {
-            if ($rows[0][0] == T_NAMESPACE) {
-                unset($rows[0]);
-                foreach ($rows as $row) {
-                    $namespace .= $row[1];
-                }
-                continue;
-            }
-
-            $results[$i]['class'] = '';
-
-            // attach the current namespace if it does not begin with '\'
-            if ($rows[0][1] != '\\') {
-                $results[$i]['class'] = $namespace ? $namespace.'\\' : '';
-            }
-
-            foreach ($rows as $row) {
-                if (self::isBuiltinType($row[1])) {
-                    unset($results[$i]);
-                    continue;
-                }
-                if ($rows[0][1] != '\\') {
-                    if (isset(array_values($imports)[0][$rows[0][1]][0])) {
-                        $results[$i]['class'] = array_values($imports)[0][$rows[0][1]][0];
-                    } else {
-                        $results[$i]['class'] .= $row[1];
-                    }
-                } else {
-                    $results[$i]['class'] .= $row[1];
-                }
-                $results[$i]['line'] = $row[2];
-            }
-        }
-
-        return $results;
+        dump('=====================================');
+        dump('was not able to properly parse the: '.$absFilePath.' file.');
+        dump('Please open up an issue on the github repo');
+        dump('https://github.com/imanghafoori1/laravel-microscope/issues');
+        dump('and also send the content of the file to the maintainer to fix the issue.');
+        dump('=============== Thanks ===============');
+        sleep(3);
     }
 }
