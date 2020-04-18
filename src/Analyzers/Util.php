0392e3e4101a2872da58ead074e6073c4a5896f0
Iman <imanghafoori1@gmail.com>
2020-04-18 20:12:15 +0430

    add cache to psr4 reader

--- src/Analyzers/Util.php
+++ src/Analyzers/Util.php
@@ -4,10 +4,18 @@ namespace Imanghafoori\LaravelMicroscope\Analyzers;
 
 class Util
 {
+    private static $result = [];
+
     public static function parseComposerJson($key)
     {
+        if (isset(self::$result[$key])) {
+            return self::$result[$key];
+        }
+
         $composer = json_decode(file_get_contents(app()->basePath('composer.json')), true);
 
-        return (array) data_get($composer, $key);
+        self::$result[$key] = (array) data_get($composer, $key);
+
+        return self::$result[$key];
     }
 }
