07f55dd7cea9e3ae1540025e37327530102ce0b0
Iman <imanghafoori1@gmail.com>
2020-05-07 02:35:18 +0430

    added compact command

---
+++ src/Commands/CheckCompact.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Imanghafoori\LaravelMicroscope\Commands;
+
+use Illuminate\Console\Command;
+use Imanghafoori\LaravelMicroscope\Analyzers\Ifs;
+use Imanghafoori\LaravelMicroscope\Analyzers\FilePath;
+use Imanghafoori\LaravelMicroscope\SpyClasses\RoutePaths;
+use Imanghafoori\LaravelMicroscope\Analyzers\FunctionCall;
+use Imanghafoori\LaravelMicroscope\Analyzers\ComposerJson;
+use Imanghafoori\LaravelMicroscope\ErrorTypes\CompactCall;
+
+class CheckCompact extends Command
+{
+    protected $signature = 'check:compact';
+
+    protected $description = 'Checks the compact() function calls to be correct';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->info('Checking compact() calls, fast and furious !!!  mm(~_~)mm  ');
+
+        $this->checkRoutePaths();
+        $this->checkPsr4Classes();
+
+        event('microscope.finished.checks', [$this]);
+    }
+
+    private function checkPathForCompact($absPath)
+    {
+        $tokens = token_get_all(file_get_contents($absPath));
+
+        foreach($tokens as $i => $token) {
+            if ($tokens[$i][0] != T_FUNCTION) {
+                continue;
+            }
+            
+            $methodBody = $this->readMethodBodyAsTokens($tokens, $i);
+           
+            if ($methodBody === false) {
+                continue;
+            }
+
+            $signatureVars = $this->collectSignatureVars($tokens, $i);
+            $this->checkMethodBodyForCompact($absPath, $methodBody, $signatureVars);
+        }
+    }
+
+    private function checkRoutePaths()
+    {
+        foreach (RoutePaths::get() as $filePath) {
+            $this->checkPathForCompact($filePath);
+        }
+    }
+
+    private function checkPsr4Classes()
+    {
+        $psr4 = ComposerJson::readKey('autoload.psr-4');
+
+        foreach ($psr4 as $_namespace => $dirPath) {
+            foreach (FilePath::getAllPhpFiles($dirPath) as $filePath) {
+                $this->checkPathForCompact($filePath->getRealPath());
+            }
+        }
+    }
+
+    private function checkMethodBodyForCompact($absPath, $methodBody, $vars)
+    {
+        foreach ($methodBody as $c => $token) {
+            ($token[0] == T_VARIABLE) && $vars[$token[1]] = null;
+
+            if (! ($pp = FunctionCall::isGlobalCall('compact', $methodBody, $c))) {
+                continue;
+            }
+
+            [, $compactedVars,] = Ifs::readCondition($methodBody, $pp);
+            $compactVars = [];
+            foreach ($compactedVars as $uu => $var) {
+                $var[0] == T_CONSTANT_ENCAPSED_STRING && $compactVars[] = '$'.trim($var[1], '\'\"');
+            }
+            $compactVars = array_flip($compactVars);
+
+            unset($vars['$this']);
+            $missingVars = array_diff_key($compactVars, $vars);
+            $missingVars && CompactCall::isMissing($absPath, $methodBody[$pp][2], $missingVars);
+        }
+    }
+
+    private function collectSignatureVars(array $tokens, $i): array
+    {
+        [, $signatures,] = Ifs::readCondition($tokens, $i);
+
+        $vars = [];
+        foreach ($signatures as $sig) {
+            ($sig[0] == T_VARIABLE) && $vars[$sig[1]] = null;
+        }
+
+        return $vars;
+    }
+
+    private function readMethodBodyAsTokens($tokens, $i)
+    {
+        // fast-forward to the start of function body
+        [$char, $methodBodyStartIndex] = FunctionCall::forwardTo($tokens, $i, ['{', ';']);
+
+        // in order to avoid checking abstract methods (with no body) and do/while
+        if ($char === ';') {
+            return false;
+        }
+
+        try {
+            // fast-forward to the end of function body
+            [$methodBody,] = FunctionCall::readBody($tokens, $methodBodyStartIndex);
+
+            return $methodBody;
+        } catch (\Exception $e) {
+            // this is a work around for tokenizer weird bugs,
+            // which makes it impossible to process the syntax.
+            // so, we just ignore the current scope and continue.
+            return false;
+        }
+    }
+}
---
+++ src/ErrorTypes/CompactCall.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Imanghafoori\LaravelMicroscope\ErrorTypes;
+
+class CompactCall
+{
+    use MicroEvent;
+}
