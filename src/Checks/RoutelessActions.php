3af3d9805e85022257679a3bc1a09293c8ec4b69
imanghafoori <imanghafoori1@gmail.com>
2020-05-10 06:53:38 +0430

    refactor

---
+++ src/Checks/RoutelessActions.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Imanghafoori\LaravelMicroscope\Checks;
+
+use Illuminate\Routing\Controller;
+use Imanghafoori\LaravelMicroscope\Analyzers\FilePath;
+use Imanghafoori\LaravelMicroscope\Analyzers\ComposerJson;
+use Imanghafoori\LaravelMicroscope\Analyzers\ClassMethods;
+use Imanghafoori\LaravelMicroscope\Analyzers\NamespaceCorrector;
+
+class RoutelessActions
+{
+    public function check($errorPrinter)
+    {
+        $psr4 = ComposerJson::readKey('autoload.psr-4');
+
+        foreach ($psr4 as $psr4Namespace => $psr4Path) {
+            $files = FilePath::getAllPhpFiles($psr4Path);
+            foreach ($files as $classFilePath) {
+                $absFilePath = $classFilePath->getRealPath();
+                $tokens = token_get_all(file_get_contents($absFilePath));
+
+                $fullNamespace = $this->getFullNamespace($classFilePath, $psr4Path, $psr4Namespace);
+
+                $this->checkControllerActionsForRoutes($errorPrinter, $fullNamespace, $tokens, $absFilePath);
+
+                CheckRouteCalls::check($tokens, $absFilePath);
+            }
+        }
+    }
+
+    private function getControllerActions($methods)
+    {
+        $orphanMethods = [];
+        foreach ($methods as $method) {
+            // we exclude non-public methods
+            if ($method['visibility'][0] !== T_PUBLIC) {
+                continue;
+            }
+
+            $methodName = $method['name'][1];
+            // we exclude __construct
+            if ($methodName == '__construct') {
+                continue;
+            }
+
+            ($methodName == '__invoke') && ($methodName = '');
+
+            $methodName && ($methodName = '@'.$methodName);
+
+            $orphanMethods[] = $method;
+        }
+
+        return $orphanMethods;
+    }
+
+    private function getNamespacedClassName($classFilePath, $psr4Path, $psr4Namespace)
+    {
+        $absFilePath = $classFilePath->getRealPath();
+        $className = $classFilePath->getFilename();
+        $relativePath = str_replace(base_path(), '', $absFilePath);
+        $namespace = NamespaceCorrector::calculateCorrectNamespace($relativePath, $psr4Path, $psr4Namespace);
+
+        return $namespace.'\\'.$className;
+    }
+
+    private function isLaravelController($fullNamespace)
+    {
+        try {
+            return is_subclass_of($fullNamespace, Controller::class);
+        } catch (\Throwable $r) {
+            // it means the file does not contain a class or interface.
+            return false;
+        }
+    }
+
+    private function getFullNamespace($classFilePath, $psr4Path, $psr4Namespace)
+    {
+        $fullNamespace = $this->getNamespacedClassName($classFilePath, $psr4Path, $psr4Namespace);
+        $fullNamespace = trim($fullNamespace, '.php');
+
+        return $fullNamespace;
+    }
+
+    private function checkActions($tokens, $fullNamespace)
+    {
+        $class = ClassMethods::read($tokens);
+
+        $methods = $this->getControllerActions($class['methods']);
+        $routelessActions = [];
+        foreach ($methods as $method) {
+            $classAtMethod = trim($fullNamespace, '\\').'@'.$method['name'][1];
+            if (! app('router')->getRoutes()->getByAction($classAtMethod)) {
+                $line = $method['name'][2];
+                $routelessActions[] = [$line, $classAtMethod];
+            }
+        }
+
+        return $routelessActions;
+    }
+
+    private function checkControllerActionsForRoutes($errorPrinter, $fullNamespace, $tokens, $absFilePath)
+    {
+        if ($this->isLaravelController($fullNamespace)) {
+            $actions = $this->checkActions($tokens, $fullNamespace);
+
+            foreach ($actions as $action) {
+                $errorPrinter->routelessAction($absFilePath, $action[0], $action[1]);
+            }
+        }
+    }
+}
--- src/Commands/CheckRoutes.php
+++ src/Commands/CheckRoutes.php
@@ -13,6 +13,7 @@ use Imanghafoori\LaravelMicroscope\Analyzers\FilePath;
 use Imanghafoori\LaravelMicroscope\Analyzers\ComposerJson;
 use Imanghafoori\LaravelMicroscope\Checks\CheckRouteCalls;
 use Imanghafoori\LaravelMicroscope\Analyzers\ClassMethods;
+use Imanghafoori\LaravelMicroscope\Checks\RoutelessActions;
 use Imanghafoori\LaravelMicroscope\ErrorReporters\ErrorPrinter;
 use Imanghafoori\LaravelMicroscope\Analyzers\NamespaceCorrector;
 
@@ -39,26 +40,25 @@ class CheckRoutes extends Command
 
         $routes = app(Router::class)->getRoutes()->getRoutes();
 
-        $bar = $this->output->createProgressBar(count($routes));
+//        $bar = $this->output->createProgressBar(count($routes));
+//        $bar->start();
 
-        $bar->start();
-
-        $this->checkRouteDefinitions($errorPrinter, $routes, $bar);
+        $this->checkRouteDefinitions($errorPrinter, $routes);
 
         // checks calls like this: route('admin.user')
         // in the psr-4 loaded classes.
-        $this->checkClassesForRouteCalls($errorPrinter);
+        (new RoutelessActions())->check($errorPrinter);
 
         CheckBladeFiles::applyChecks([
             [CheckRouteCalls::class, 'check'],
         ]);
 
-        $bar->finish();
+//        $bar->finish();
 
         $this->finishCommand($errorPrinter);
     }
 
-    public function getRouteId($route)
+    private function getRouteId($route)
     {
         if ($routeName = $route->getName()) {
             return 'Error on route name: '.$routeName;
@@ -67,41 +67,10 @@ class CheckRoutes extends Command
         }
     }
 
-    protected function checkClassesForRouteCalls($errorPrinter)
-    {
-        $psr4 = ComposerJson::readKey('autoload.psr-4');
-
-        foreach ($psr4 as $psr4Namespace => $psr4Path) {
-            $files = FilePath::getAllPhpFiles($psr4Path);
-            foreach ($files as $classFilePath) {
-                $absFilePath = $classFilePath->getRealPath();
-                $tokens = token_get_all(file_get_contents($absFilePath));
-
-                $fullNamespace = $this->getFullNamespace($classFilePath, $psr4Path, $psr4Namespace);
-
-                if ($this->isLaravelController($fullNamespace)) {
-                    $class = ClassMethods::read($tokens);
-
-                    $methods = $this->getControllerActions($class['methods']);
-
-                    foreach ($methods as $method) {
-                        $classAtMethod = trim($fullNamespace, '\\').'@'.$method['name'][1];
-                        if (! app('router')->getRoutes()->getByAction($classAtMethod)) {
-                            $line = $method['name'][2];
-                            $errorPrinter->routelessAction($absFilePath, $line, $classAtMethod);
-                        }
-                    }
-                }
-
-                CheckRouteCalls::check($tokens, $absFilePath);
-            }
-        }
-    }
-
-    private function checkRouteDefinitions($errorPrinter, $routes, $bar)
+    private function checkRouteDefinitions($errorPrinter, $routes)
     {
         foreach ($routes as $route) {
-            $bar->advance();
+//            $bar->advance();
 
             if (! is_string($ctrl = $route->getAction()['uses'])) {
                 continue;
@@ -125,65 +94,5 @@ class CheckRoutes extends Command
                 $errorPrinter->route($ctrl, $msg1, $msg2);
             }
         }
-    }
-
-    private function getControllerActions($methods)
-    {
-        $orphanMethods = [];
-        foreach ($methods as $method) {
-            // we exclude non-public methods
-            if ($method['visibility'][0] !== T_PUBLIC) {
-                continue;
-            }
-
-            $methodName = $method['name'][1];
-            // we exclude __construct
-            if ($methodName == '__construct') {
-                continue;
-            }
-
-            ($methodName == '__invoke') && ($methodName = '');
-
-            $methodName && ($methodName = '@'.$methodName);
-
-            $orphanMethods[] = $method;
-        }
-
-        return $orphanMethods;
-    }
-
-    protected function getNamespacedClassName($classFilePath, $psr4Path, $psr4Namespace)
-    {
-        $absFilePath = $classFilePath->getRealPath();
-        $className = $classFilePath->getFilename();
-        $relativePath = str_replace(base_path(), '', $absFilePath);
-        $namespace = NamespaceCorrector::calculateCorrectNamespace($relativePath, $psr4Path, $psr4Namespace);
-
-        return $namespace.'\\'.$className;
-    }
-
-    protected function isLaravelController($fullNamespace)
-    {
-        try {
-            return is_subclass_of($fullNamespace, Controller::class);
-        } catch (\Throwable $r) {
-            // it means the file does not contain a class or interface.
-            return false;
-        }
-    }
-
-    /**
-     * @param $classFilePath
-     * @param $psr4Path
-     * @param $psr4Namespace
-     *
-     * @return string
-     */
-    protected function getFullNamespace($classFilePath, $psr4Path, $psr4Namespace)
-    {
-        $fullNamespace = $this->getNamespacedClassName($classFilePath, $psr4Path, $psr4Namespace);
-        $fullNamespace = trim($fullNamespace, '.php');
-
-        return $fullNamespace;
     }
 }
