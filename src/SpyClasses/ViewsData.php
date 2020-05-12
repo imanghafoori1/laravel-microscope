cc461303c6df91215888f68d9681ffc676cbfcd3
imanghafoori <imanghafoori1@gmail.com>
2020-05-13 03:42:59 +0430

    fix override variables issue

--- src/SpyClasses/ViewsData.php
+++ src/SpyClasses/ViewsData.php
@@ -2,6 +2,8 @@
 
 namespace Imanghafoori\LaravelMicroscope\SpyClasses;
 
+use Imanghafoori\LaravelMicroscope\Analyzers\FunctionCall;
+
 class ViewsData
 {
     public $all = [];
@@ -31,8 +33,14 @@ class ViewsData
         foreach ($this->all as $view) {
             $vars = [];
             $tokens = token_get_all(app('blade.compiler')->compileString(file_get_contents($view->getPath())));
-            foreach ($tokens as $token) {
-                ($token[0] == T_VARIABLE) && $vars[$token[1]] = null;
+            foreach ($tokens as $i => $token) {
+                // we collect only vars which are not being declared.
+                if (
+                    $token[0] == T_VARIABLE
+                    &&
+                    FunctionCall::getNextToken($tokens, $i)[0] !== '='
+                ) {
+                } $vars[$token[1]] = null;
             }
             $allVars = $allVars + $vars;
         }
