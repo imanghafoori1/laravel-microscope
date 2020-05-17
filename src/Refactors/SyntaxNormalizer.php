50ae2c06ff039be07f4fe52b798044ddfb00eb08
imanghafoori <imanghafoori1@gmail.com>
2020-05-17 09:41:47 +0430

    added support for if with no curly brace

--- src/Refactors/SyntaxNormalizer.php
+++ src/Refactors/SyntaxNormalizer.php
@@ -18,17 +18,52 @@ class SyntaxNormalizer
         while ($tCount > $i) {
             $t = $tokens[$i];
             if (in_array($t[0], $ends)) {
+                // replace the ruby-style syntax with C-style
                 $refactoredTokens[] = ['}', $t[1]];
                 $i++;
                 continue;
             }
+
             if (in_array($t[0], $start)) {
+                // forward to end of parenthesis
                 [, , $u] = Ifs::readCondition($tokens, $i);
+                // read first char after the parenthesis
                 [$next, $u] = FunctionCall::getNextToken($tokens, $u);
                 if ($next == ':') {
                     $tokens[$u] = ['{', ':'];
                     // Adds a closing curly brace "}" before elseif.
                     $t[0] == T_ELSEIF && $refactoredTokens[] = ['}', ''];
+                }
+            }
+
+            if ($t[0] == T_ELSE || $t[0] == T_IF) {
+                if ($t[0] == T_ELSE) {
+                    [$next_T, $next_I] = FunctionCall::getNextToken($tokens, $i);
+                } else {
+                    [, , $u] = Ifs::readCondition($tokens, $i);
+                    [$next_T, $next_I] = FunctionCall::getNextToken($tokens, $u);
+                }
+
+                if (in_array($next_T[0], [T_FOR, T_FOREACH, T_WHILE])) {
+                    array_splice($tokens, $next_I, 0, ['{']);
+                    $refactoredTokens[] = $t;
+                    $i++;
+                    [, , $u] = Ifs::readCondition($tokens, $next_I + 1);
+                    [, $u] = FunctionCall::getNextToken($tokens, $u);
+                    [, $u] = FunctionCall::readBody($tokens, $u);
+                    array_splice($tokens, $u, 0, ['}']);
+
+                    // we update the count since the number of elements is changed.
+                    $tCount = count($tokens);
+                    continue;
+                } elseif ($next_T[0] !== T_IF && $next_T !== '{') {
+                    $refactoredTokens[] = $t;
+                    $refactoredTokens[] = '{';
+                    [, $endIndex] = FunctionCall::forwardTo($tokens, $i, [';']);
+                    array_splice($tokens, $endIndex + 1, 0, ['}']);
+                    $tCount = count($tokens);
+                    $i++;
+                    continue;
                 }
             }
 
