22498ce1d471cfe45d55f285b6ede1efe1419820
imanghafoori <imanghafoori1@gmail.com>
2020-05-17 08:13:50 +0430

    optimize

--- src/Refactors/SyntaxNormalizer.php
+++ src/Refactors/SyntaxNormalizer.php
@@ -13,8 +13,9 @@ class SyntaxNormalizer
         $start = [T_FOR, T_IF, T_FOREACH, T_WHILE, T_ELSEIF];
         $i = 0;
         $refactoredTokens = [];
+        $tCount = count($tokens);
 
-        while (count($tokens) > $i) {
+        while ($tCount > $i) {
             $t = $tokens[$i];
             if (in_array($t[0], $ends)) {
                 $refactoredTokens[] = ['}', $t[1]];
