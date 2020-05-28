6739e03fea7c8ead41c0ad0b3aaff50fc0af4e95
imanghafoori <imanghafoori1@gmail.com>
2020-05-28 08:52:09 +0430

    event inspection utilities

--- src/SpyClasses/SpyDispatcher.php
+++ src/SpyClasses/SpyDispatcher.php
@@ -2,15 +2,40 @@
 
 namespace Imanghafoori\LaravelMicroscope\SpyClasses;
 
+use Illuminate\Support\Str;
 use Illuminate\Events\Dispatcher;
 use Imanghafoori\LaravelMicroscope\ErrorReporters\ErrorPrinter;
 
 class SpyDispatcher extends Dispatcher
 {
+    public $originalListeners = [];
+
     public function listen($events, $listener)
     {
-        $event = (array) $events;
-        $this->validateCallback($event[0], $listener);
+        parent::listen($events, $listener);
+
+        $events = (array) $events;
+        foreach($events as $event) {
+            $this->validateCallback($event, $listener);
+        }
+
+        // do not move this loop into a private method or something, it breaks.
+        foreach ((array)$events as $event) {
+            $i = 0;
+            $excludes = [
+                base_path('vendor'.DIRECTORY_SEPARATOR.'laravel'),
+            ];
+            while (($t = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, $i + 1)[$i]) && Str::startsWith($t['file'], $excludes)) {
+                $i++;
+            }
+            unset($t['object']);
+            $this->originalListeners[$event][] = [$listener, $t];
+        }
+    }
+
+    public function getOriginalListeners($event)
+    {
+        return $this->originalListeners[$event] ?? [];
     }
 
     private function error($string)
--- src/helpers.php
+++ src/helpers.php
@@ -20,3 +20,26 @@ if (! function_exists('microscope_write_route')) {
         config()->push('microscope.write.routes', debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 1)[0]);
     }
 }
+
+if (! function_exists('microscope_dd_listeners')) {
+    function microscope_dd_listeners($event)
+    {
+        config()->push('microscope.dump.listeners', $event);
+        app()->booted(function () {
+            $events = config('microscope.dump.listeners');
+            foreach ($events as $event) {
+                $listernsInfo = Event::getOriginalListeners($event);
+                dump(' Event:  ' .$event);
+                dump(' Listeners: ');
+                $sp = '     ';
+                foreach($listernsInfo as $i => $listernInfo) {
+                    dump($sp.($i + 1).' - '.$listernInfo[0]);
+                    $relPath = \Imanghafoori\LaravelMicroscope\Analyzers\FilePath::getRelativePath($listernInfo[1]['file']);
+                    dump($sp.'    at '.$relPath. ':'.$listernInfo[1]['line']);
+                }
+                dump('------------------------------------------');
+            }
+            dd();
+        });
+    }
+}
