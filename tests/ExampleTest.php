c381ef6c40c4c07b0039fb7d55a2b8e007595d44
Iman <imanghafoori1@users.noreply.github.com>
2021-05-10 05:38:15 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -16,7 +16,7 @@ class ExampleTest extends TestCase
     /** @test */
     public function true_is_true()
     {
-        $string = file_get_contents(__DIR__. '/stubs/sample_class.php');
+        $string = file_get_contents(__DIR__.'/stubs/sample_class.php');
         $tokens = token_get_all($string);
 
         $class = ClassMethods::read($tokens);
@@ -24,26 +24,26 @@ class ExampleTest extends TestCase
         $this->assertFalse($class['is_abstract']);
         $this->assertEquals($class['type'], T_CLASS);
         $this->assertCount(8, $class['methods']);
-        $this->assertEquals($class['methods'][0]["is_abstract"], false);
-        $this->assertEquals($class['methods'][0]["is_static"], false);
-        $this->assertEquals($class['methods'][0]["returnType"], null);
-        $this->assertEquals($class['methods'][0]["nullable_return_type"], null);
-        $this->assertEquals($class['methods'][0]["signature"], []);
-        $this->assertEquals($class["methods"][0]["name"], [311, "hello1", 5]);
+        $this->assertEquals($class['methods'][0]['is_abstract'], false);
+        $this->assertEquals($class['methods'][0]['is_static'], false);
+        $this->assertEquals($class['methods'][0]['returnType'], null);
+        $this->assertEquals($class['methods'][0]['nullable_return_type'], null);
+        $this->assertEquals($class['methods'][0]['signature'], []);
+        $this->assertEquals($class['methods'][0]['name'], [311, 'hello1', 5]);
 
-        $this->assertEquals($class["methods"][0]["visibility"][1], "public");
-        $this->assertEquals($class["methods"][1]["visibility"][1], "protected");
-        $this->assertEquals($class["methods"][2]["visibility"][1], "private");
-        $this->assertEquals($class["methods"][3]["visibility"][1], "public");
+        $this->assertEquals($class['methods'][0]['visibility'][1], 'public');
+        $this->assertEquals($class['methods'][1]['visibility'][1], 'protected');
+        $this->assertEquals($class['methods'][2]['visibility'][1], 'private');
+        $this->assertEquals($class['methods'][3]['visibility'][1], 'public');
 
-        $this->assertEquals($class["methods"][4]["visibility"][1], "public");
-        $this->assertEquals($class["methods"][5]["visibility"][1], "protected");
-        $this->assertEquals($class["methods"][6]["visibility"][1], "private");
-        $this->assertEquals($class["methods"][7]["visibility"][1], "public");
+        $this->assertEquals($class['methods'][4]['visibility'][1], 'public');
+        $this->assertEquals($class['methods'][5]['visibility'][1], 'protected');
+        $this->assertEquals($class['methods'][6]['visibility'][1], 'private');
+        $this->assertEquals($class['methods'][7]['visibility'][1], 'public');
 
-        $this->assertEquals($class['methods'][4]["is_static"], true);
-        $this->assertEquals($class['methods'][5]["is_static"], true);
-        $this->assertEquals($class['methods'][6]["is_static"], true);
-        $this->assertEquals($class['methods'][7]["is_static"], true);
+        $this->assertEquals($class['methods'][4]['is_static'], true);
+        $this->assertEquals($class['methods'][5]['is_static'], true);
+        $this->assertEquals($class['methods'][6]['is_static'], true);
+        $this->assertEquals($class['methods'][7]['is_static'], true);
     }
 }
