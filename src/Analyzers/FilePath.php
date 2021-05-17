cf99234d371797a34a7b1840182e18edc4d5b2ce
hAz <claymore0465@gmail.com>
2021-05-17 23:52:57 +0430

    add some refactor and writ test for FilePath analyzer

--- src/Analyzers/FilePath.php
+++ src/Analyzers/FilePath.php
@@ -7,15 +7,22 @@ use Symfony\Component\Finder\Finder;
 
 class FilePath
 {
+    /**
+     * normalize file path to standard formal
+     * for a path like: "/usr/laravel/app\Http\..\..\database" returns "/usr/laravel/database".
+     *
+     * @param string $path directory path
+     *
+     * @return string
+     */
     public static function normalize($path)
     {
-        // for a path like:  '/usr/laravel/app\Http\..\..\database';
         $dir = \str_replace(['\\', '/', '//', '\\\\'], DIRECTORY_SEPARATOR, $path);
 
         $sections = \explode(DIRECTORY_SEPARATOR, $dir);
 
         $result = [];
-        foreach ($sections as $i => $section) {
+        foreach ($sections as $section) {
             if ($section == '..') {
                 \array_pop($result);
             } else {
@@ -26,13 +33,22 @@ class FilePath
         return \implode(DIRECTORY_SEPARATOR, $result);
     }
 
+    /**
+     * get relative path. removes base path of laravel installation from an absolute path.
+     *
+     * @param string $absFilePath Absolute directory path
+     *
+     * @return string
+     */
     public static function getRelativePath($absFilePath)
     {
         return \trim(Str::replaceFirst(base_path(), '', $absFilePath), DIRECTORY_SEPARATOR);
     }
 
     /**
-     * @param $path
+     * get all ".php" files in directory by giving a path.
+     *
+     * @param string $path Directory path
      *
      * @return \Symfony\Component\Finder\Finder
      */
