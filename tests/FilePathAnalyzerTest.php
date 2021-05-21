40185b97d1912076cad54027df0a99316c2f8bfb
Iman Ghafoori <imanghafoori1@gmail.com>
2021-05-21 11:48:09 +0430

    add test

--- tests/FilePathAnalyzerTest.php
+++ tests/FilePathAnalyzerTest.php
@@ -10,9 +10,14 @@ class FilePathAnalyzerTest extends BaseTestClass
     public function method_normalize_test()
     {
         $path = '/usr/laravel\\\\framework/app\Http\..\..\\..//database';
+        $path2 = '\usr\laravel\framework/app\Http\..\..\\..//database';
+        $path3 = '\usr\laravel\..\framework/app\Http\..\\..//database';
         $normalizedPath = FilePath::normalize($path);
+        $ds = DIRECTORY_SEPARATOR;
 
-        $this->assertEquals(implode(DIRECTORY_SEPARATOR, ['', 'usr', 'laravel', 'database']), $normalizedPath);
+        $this->assertEquals( "{$ds}usr{$ds}laravel{$ds}database", $normalizedPath);
+        $this->assertEquals( "{$ds}usr{$ds}laravel{$ds}database", FilePath::normalize($path2));
+        $this->assertEquals( "{$ds}usr{$ds}framework{$ds}database", FilePath::normalize($path3));
         $this->assertStringNotContainsString('\\\\', $normalizedPath);
         $this->assertStringNotContainsString('//', $normalizedPath);
         $this->assertStringNotContainsString('..', $normalizedPath);
