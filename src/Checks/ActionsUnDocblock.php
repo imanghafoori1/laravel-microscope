79ab71f74a2d33c8bd0a0735068284cd352308ac
AliGhorbani <alighorbani20002@gmail.com>
2021-06-09 10:54:21 +0430

    combine two if and little refactor

--- src/Checks/ActionsUnDocblock.php
+++ src/Checks/ActionsUnDocblock.php
@@ -7,8 +7,20 @@ use Imanghafoori\LaravelMicroscope\Analyzers\Refactor;
 
 class ActionsUnDocblock
 {
+    public const LARAVEL_DOCBLOCK_CONTENT =
+    [
+        'Remove the specified resource from storage.',
+        'Update the specified resource in storage.',
+        'Store a newly created resource in storage.',
+        'Display the specified resource.',
+        'Display a listing of the resource.',
+        'Show the form for creating a new resource.',
+        'Show the form for editing the specified resource.',
+    ];
+
     public static $command;
 
+    
     public static function check($tokens, $absFilePath, $classFilePath, $psr4Path, $psr4Namespace)
     {
         $fullNamespace = RoutelessActions::getFullNamespace($classFilePath, $psr4Path, $psr4Namespace);
@@ -18,19 +30,15 @@ class ActionsUnDocblock
         }
     }
 
+    private static function isValidDockBlock($tokenType, $tokenContent)
+    {
+        return $tokenType == T_DOC_COMMENT && Str::contains($tokenContent, self::LARAVEL_DOCBLOCK_CONTENT);
+    }
+
     private static function removeGenericDocBlocks($tokens, $absFilePath)
     {
         foreach ($tokens as $i => $token) {
-            if ($token[0] == T_DOC_COMMENT) {
-                if (Str::contains($token[1], [
-                    'Remove the specified resource from storage.',
-                    'Update the specified resource in storage.',
-                    'Store a newly created resource in storage.',
-                    'Display the specified resource.',
-                    'Display a listing of the resource.',
-                    'Show the form for creating a new resource.',
-                    'Show the form for editing the specified resource.',
-                ])) {
+            if (self::isValidDockBlock($token(0), $token(1))) {
                     unset($tokens[$i]);
                     if ($tokens[$i + 1][0] == T_WHITESPACE) {
                         unset($tokens[$i + 1]);
