7be27b55cbb0a7e128e2706a86077ee72479d3ea
Iman Ghafoori <imanghafoori1@gmail.com>
2021-06-18 17:42:32 +0430

    code clean up

--- src/Stubs/ServiceProviderStub.php
+++ src/Stubs/ServiceProviderStub.php
@@ -6,13 +6,14 @@ class ServiceProviderStub
 {
     public static function providerContent($correctNamespace, $className, $prefix)
     {
-        $string = file_get_contents(__DIR__.'/microscopeServiceProvider.stub');
+        $template = file_get_contents(__DIR__.'/microscopeServiceProvider.stub');
+
         $mapping = [
             '$correctNamespace' => $correctNamespace,
             '$className' => $className,
             '$name' => $prefix,
         ];
 
-        return '<?php'.str_replace(array_keys($mapping), array_values($mapping), $string);
+        return '<?php'.str_replace(array_keys($mapping), array_values($mapping), $template);
     }
 }
