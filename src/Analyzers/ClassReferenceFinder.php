49fa89f8ce1a4b9523d1bf021be96bb5c439ff15
Iman Ghafoori <imanghafoori1@gmail.com>
2021-07-11 03:07:32 +0430

    php8 compatibility

--- src/Analyzers/ClassReferenceFinder.php
+++ src/Analyzers/ClassReferenceFinder.php
@@ -249,6 +249,8 @@ class ClassReferenceFinder
             'protected',
             'float',
             'void',
+            'false',
+            'true',
             'null',
             'bool',
             'array',
