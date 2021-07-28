1c86744788a649c5a635d8b0b3fcb0098d18ba91
Iman Ghafoori <imanghafoori1@gmail.com>
2021-07-28 18:44:32 +0430

    remove stubs

--- tests/stubs/refactor_patterns.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    "if (!'<variable>' && '<boolean>') { return response()->'<name>'(['message' => __('<string>')], '<number>'); }" => ['replace' => 'Foo::bar("<1>", "<2>", "<3>"(), "<4>");'],
-
-    'foo(false, true, null);' => ['replace' => 'bar("hi");'],
-];
