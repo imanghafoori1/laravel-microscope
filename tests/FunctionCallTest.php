7b26a8e2663a873b2caffdbb6056e009162b58b7
abofazl rasoli <75317352+abolfazl-rasoli@users.noreply.github.com>
2022-04-22 22:29:34 +0430

    fix styleci issue

--- tests/FunctionCallTest.php
+++ tests/FunctionCallTest.php
@@ -9,16 +9,16 @@ class FunctionCallTest extends BaseTestClass
     /** @test */
     public function has_dd_test()
     {
-     
-        $tokens = token_get_all(file_get_contents(__DIR__ . "/stubs/function_test/some_function.sub"));
+        $tokens = token_get_all(file_get_contents(__DIR__.'/stubs/function_test/some_function.sub'));
 
         $index = null;
-        foreach($tokens as $i => $token) 
-            if( $index = FunctionCall::isGlobalCall("dd", $tokens, $i)) break;
+        foreach($tokens as $i => $token) {
+            if( $index = FunctionCall::isGlobalCall('dd', $tokens, $i)) {
+                break;
+            }
+        }
 
         $this->assertNotNull($index);
         $this->assertEquals(27, $index);
-
     }
-
 }
