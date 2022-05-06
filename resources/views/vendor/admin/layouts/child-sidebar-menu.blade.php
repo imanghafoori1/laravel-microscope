6fed0d0f3012b1ab66e7843c897cbb1dc1f69bf1
ali khosravi <alikhosravi175@gmail.com>
2022-05-06 15:11:43 +0430

    clean files

--- resources/views/vendor/admin/layouts/child-sidebar-menu.blade.php
+++
@@ -1,5 +0,0 @@
-<li class="list-divider"></li>
-<li class="nav-small-cap"><span class="hide-menu">{{ __('CRUD Menu') }}</span></li>
-@foreach(\EasyPanel\Models\CRUD::active() as $crud)
-    <x-easypanel::crud-menu-item :crud="$crud" />
-@endforeach
