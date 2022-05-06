0942ee67277a1e40a05e35725549002131ba43f1
ali khosravi <alikhosravi175@gmail.com>
2022-05-06 21:00:54 +0430

    require laravel-microscope

---
+++ resources/views/vendor/microscope/action_comment.blade.php
@@ -0,0 +1,19 @@
+@php
+    $all_methods = ['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'];
+    ($all_methods == $methods) && ($methods = ['any']);
+@endphp
+      /**
+@if (\count($methods) > 1)
+        * @methods('{!! \implode("', ", $methods) !!}')
+        * @uri('{!! $url !!}')
+@else
+        * {!! '@'.strtolower(\implode('', $methods)) !!}('{!! $url !!}')
+@endif
+@if($routeName)
+        * @name('{!! $routeName !!}')
+@endif
+@if ($file)
+        * @at({!! $file !!}:{!! $line !!})
+@endif
+        * @middlewares('{!! implode("', '", $middlewares) !!}')
+        */
