b104b2e738db7457ae2caa8bc17ea1e3e43bd821
Iman Ghafoori <imanghafoori1@gmail.com>
2022-06-30 17:26:16 +0430

    fix self for is_sub_class_of

--- src/SearchReplace/IsSubClassOf.php
+++ src/SearchReplace/IsSubClassOf.php
@@ -2,13 +2,19 @@
 
 namespace Imanghafoori\LaravelMicroscope\SearchReplace;
 
+use Imanghafoori\TokenAnalyzer\GetClassProperties;
 use Imanghafoori\TokenAnalyzer\ParseUseStatement;
 
 class IsSubClassOf
 {
     public static function check($placeholderVal, $parameter, $tokens)
     {
-        $fullClassPath = ParseUseStatement::getExpandedRef($tokens, $placeholderVal[1]);
+        if ($placeholderVal[1] === 'self') {
+            [$namespace, $class] = GetClassProperties::readClassDefinition($tokens);
+            $fullClassPath = $namespace.'\\'.$class;
+        } else {
+            $fullClassPath = ParseUseStatement::getExpandedRef($tokens, $placeholderVal[1]);
+        }
 
         return is_subclass_of($fullClassPath, $parameter);
     }
