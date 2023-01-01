c887e5958fcd5ed0b365e6c5f686bf03329f550c
Iman Ghafoori <imanghafoori1@gmail.com>
2023-01-02 00:18:17 +0330

    Tweak check:import report

--- src/ErrorReporters/CheckImportReporter.php
+++ src/ErrorReporters/CheckImportReporter.php
@@ -12,7 +12,7 @@ class CheckImportReporter
 {
     public static function report(CheckImports $command, array $psr4Stats, array $foldersStats, array $bladeStats, int $countRouteFiles): void
     {
-        $command->getOutput()->writeln('<options=bold;fg=yellow>'.CheckClassReferences::$refCount.' import'.(CheckClassReferences::$refCount == 1 ? '' : 's').' were checked under:</>');
+        $command->getOutput()->writeln('<options=bold;fg=yellow>'.CheckClassReferences::$refCount.' imports were checked under:</>');
 
         self::printPsr4($psr4Stats, $command);
 
@@ -38,6 +38,58 @@ class CheckImportReporter
         }
         $output .= ')'.PHP_EOL;
 
+        $output = self::foldersStats($foldersStats, $output);
+
+        $output .= '   - <fg=blue>'.$countRouteFiles.'</> route'.($countRouteFiles <= 1 ? '' : 's').PHP_EOL;
+        $command->line($output);
+
+        return $output;
+    }
+
+    private static function printErrorsCount(CheckImports $command): void
+    {
+        $totalErrors = CheckClassReferences::$unusedImportsCount + CheckClassReferences::$wrongImportsCount;
+        $output = '<options=bold;fg=yellow>'.CheckClassReferences::$refCount.' refs were checked, '.$totalErrors.' error'.($totalErrors == 1 ? '' : 's').' found.</>'.PHP_EOL;
+        $output .= ' - <fg=yellow>'.CheckClassReferences::$unusedImportsCount.' unused</> import'.(CheckClassReferences::$unusedImportsCount == 1 ? '' : 's').' found.'.PHP_EOL;
+        $output .= ' - <fg=red>'.CheckClassReferences::$wrongImportsCount.' wrong</> import'.(CheckClassReferences::$wrongImportsCount <= 1 ? '' : 's').' found.'.PHP_EOL;
+        $output .= ' - <fg=red>'.CheckClassReferences::$wrongClassRefCount.' wrong</> class'.(CheckClassReferences::$wrongClassRefCount <= 1 ? '' : 'es').' ref found.';
+        $command->getOutput()->writeln($output);
+    }
+
+    private static function printPsr4(array $psr4Stats, CheckImports $command): void
+    {
+        $spaces = self::getMaxLength($psr4Stats);
+
+        foreach ($psr4Stats as $composerPath => $psr4) {
+            $composerPath = trim($composerPath, '/');
+            $composerPath = $composerPath ? trim($composerPath, '/').'/' : '';
+            $output = ' <fg=blue>./'.$composerPath.'composer.json'.'</>'.PHP_EOL;
+            foreach ($psr4 as $psr4Namespace => $psr4Paths) {
+                foreach ($psr4Paths as $path => $countClasses) {
+                    $countClasses = str_pad((string) $countClasses, 3, ' ', STR_PAD_LEFT);
+                    $len = strlen($psr4Namespace);
+                    $output .= '   - <fg=red>'.$psr4Namespace.str_repeat(' ', $spaces - $len).' </>';
+                    $output .= " <fg=blue>$countClasses </>file".($countClasses == 1 ? '' : 's').' found (<fg=green>./'.$path."</>)\n";
+                }
+            }
+            $command->getOutput()->writeln($output);
+        }
+    }
+
+    private static function getMaxLength(array $psr4Stats)
+    {
+        $lens = [];
+        foreach ($psr4Stats as $psr4) {
+            foreach ($psr4 as $psr4Namespace => $psr4Paths) {
+                $lens[] = strlen($psr4Namespace);
+            }
+        }
+
+        return max($lens);
+    }
+
+    private static function foldersStats($foldersStats, string $output)
+    {
         foreach ($foldersStats as $fileType => $stats) {
             $output .= '   - <fg=blue>'.$stats['fileCount'].'</> '.$fileType;
             if (empty($stats['paths'])) {
@@ -49,7 +101,7 @@ class CheckImportReporter
             $numPaths = count($paths);
             $i = 0;
             foreach ($paths as $path) {
-                $isEnd = end($paths) == $path;
+                //$isEnd = end($paths) == $path;
                 $path = FilePath::normalize(str_replace(base_path(), '.', $path));
                 $output .= '<fg=green>'.$path.'</>';
                 if (++$i !== $numPaths) {
@@ -59,36 +111,6 @@ class CheckImportReporter
             $output .= ')'.PHP_EOL;
         }
 
-        $output .= '   - <fg=blue>'.$countRouteFiles.'</> route'.($countRouteFiles <= 1 ? '' : 's').PHP_EOL;
-        $command->line($output);
-
         return $output;
-    }
-
-    private static function printErrorsCount(CheckImports $command): void
-    {
-        $totalErrors = CheckClassReferences::$unusedImportsCount + CheckClassReferences::$wrongImportsCount;
-        $output = '<options=bold;fg=yellow>'.$totalErrors.' error'.($totalErrors == 1 ? '' : 's').' found.</>'.PHP_EOL;
-        $output .= ' - <fg=yellow>'.CheckClassReferences::$unusedImportsCount.' unused</> import'.(CheckClassReferences::$unusedImportsCount == 1 ? '' : 's').' found.'.PHP_EOL;
-        $output .= ' - <fg=red>'.CheckClassReferences::$wrongImportsCount.' wrong</> import'.(CheckClassReferences::$wrongImportsCount <= 1 ? '' : 's').' found.'.PHP_EOL;
-        $output .= ' - <fg=red>'.CheckClassReferences::$wrongClassRefCount.' wrong</> class'.(CheckClassReferences::$wrongClassRefCount <= 1 ? '' : 'es').' ref found.';
-        $command->getOutput()->writeln($output);
-    }
-
-    private static function printPsr4(array $psr4Stats, CheckImports $command): void
-    {
-        $len = 0;
-        foreach ($psr4Stats as $composerPath => $psr4) {
-            $output = ' <fg=blue>./'.trim($composerPath.'/', '/').'composer.json'.'</>'.PHP_EOL;
-            foreach ($psr4 as $psr4Namespace => $psr4Paths) {
-                foreach ($psr4Paths as $path => $countClasses) {
-                    $max = max($len, strlen($psr4Namespace));
-                    $len = strlen($psr4Namespace);
-                    $output .= '   - <fg=red>'.$psr4Namespace.str_repeat(' ', $max - $len).' </>';
-                    $output .= " <fg=blue>$countClasses </>class".($countClasses == 1 ? '' : 'es').' found (<fg=green>./'.$path."</>)\n";
-                }
-            }
-            $command->getOutput()->writeln($output);
-        }
     }
 }
