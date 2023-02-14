c0d5eb94ac357fc947c7eb56a1d5e2443a79d55f
Iman Ghafoori <imanghafoori1@gmail.com>
2023-02-15 02:12:40 +0330

    refactor

--- src/Psr4/CheckNamespaces.php
+++ src/Psr4/CheckNamespaces.php
@@ -16,29 +16,13 @@ class CheckNamespaces
         return $errorsLists;
     }
 
-    public static function checkNamespace($relativePath, $psr4Mapping, $currentNamespace, $class, $fileName)
-    {
-        $correctNamespaces = NamespaceCalculator::getCorrectNamespaces($psr4Mapping, $relativePath);
-
-        if (! in_array($currentNamespace, $correctNamespaces)) {
-            return [
-                'type' => 'namespace',
-                'correctNamespace' => self::findShortest($correctNamespaces),
-            ];
-        } elseif (($class.'.php') !== $fileName) {
-            return [
-                'type' => 'filename',
-            ];
-        }
-    }
-
     public static function findPsr4Errors($basePath, $psr4Mapping, $classes, $composerPath, ?\Closure $onCheck)
     {
         $errors = [];
         foreach ($classes as $class) {
             $onCheck && $onCheck($class);
             $relativePath = \trim(str_replace($basePath, '', $class['absFilePath']), '/\\');
-            $error = self::checkNamespace($relativePath, $psr4Mapping, $class['currentNamespace'], $class['class'], $class['fileName']);
+            $error = NamespaceCalculator::checkNamespace($relativePath, $psr4Mapping, $class['currentNamespace'], $class['class'], $class['fileName']);
 
             if ($error) {
                 $error['relativePath'] = $relativePath;
@@ -48,17 +32,5 @@ class CheckNamespaces
         }
 
         return $errors;
-    }
-
-    private static function findShortest($correctNamespaces)
-    {
-        // finds the shortest namespace
-        return array_reduce($correctNamespaces, function ($a, $b) {
-            if ($a === null) {
-                return $b;
-            }
-
-            return strlen($a) < strlen($b) ? $a : $b;
-        });
     }
 }
