6b6d165cf013d186066f3f52a34bf207fdd77aa6
ali <alimirzaee1382@gmail.com>
2023-02-21 16:34:39 +0330

    refactor 1

--- src/Checks/PSR12/CurlyBraces.php
+++ src/Checks/PSR12/CurlyBraces.php
@@ -27,56 +27,56 @@ class CurlyBraces
                 $level--;
                 ($isInSideClass === true && $level === 0) && ($isInSideClass = false);
             }
-            if ($level === 0) {
-                if (\in_array($token[0], [T_CLASS, T_TRAIT, T_INTERFACE])) {
-                    if ($tokens[$i - 1] !== T_DOUBLE_COLON) {
-                        $isInSideClass = true;
-                    }
-                }
-            }
-            self::openCurly($token, $level, $tokens, $i, $classFilePath);
-
-            [$tokens, $i] = self::writePublic($level, $token, $isInSideClass, $i, $tokens, $classFilePath);
+            $condition = $level === 0
+                && \in_array($token[0], [T_CLASS, T_TRAIT, T_INTERFACE])
+                && $tokens[$i - 1] !== T_DOUBLE_COLON;
+            $isInSideClass = $condition ?  true : $isInSideClass;
         }
-    }
+        self::openCurly($token, $level, $tokens, $i, $classFilePath);
 
-    private static function openCurly($token, $level, $tokens, $i, $classFilePath)
-    {
-        if ($token == '{' && ! \in_array($tokens[$i - 1][0], [T_DOUBLE_COLON, T_OBJECT_OPERATOR])) {
-            $sp = str_repeat('    ', $level);
-            if ($tokens[$i + 1][0] === T_WHITESPACE) {
-                if ($tokens[$i + 1][1] !== PHP_EOL.$sp && $tokens[$i + 1][1] !== "\n".$sp) {
-                    $tokens[$i + 1][1] = PHP_EOL.$sp;
-                    Refactor::saveTokens($classFilePath, $tokens);
-                } else {
-                    //
-                }
-            } else {
-                array_splice($tokens, $i + 1, 0, [[T_WHITESPACE, PHP_EOL.$sp]]);
+        [$tokens, $i] = self::writePublic($level, $token, $isInSideClass, $i, $tokens, $classFilePath);
+    }
+}
+
+private
+static function openCurly($token, $level, $tokens, $i, $classFilePath)
+{
+    if ($token == '{' && !\in_array($tokens[$i - 1][0], [T_DOUBLE_COLON, T_OBJECT_OPERATOR])) {
+        $sp = str_repeat('    ', $level);
+        if ($tokens[$i + 1][0] === T_WHITESPACE) {
+            if ($tokens[$i + 1][1] !== PHP_EOL . $sp && $tokens[$i + 1][1] !== "\n" . $sp) {
+                $tokens[$i + 1][1] = PHP_EOL . $sp;
                 Refactor::saveTokens($classFilePath, $tokens);
+            } else {
+                //
             }
+        } else {
+            array_splice($tokens, $i + 1, 0, [[T_WHITESPACE, PHP_EOL . $sp]]);
+            Refactor::saveTokens($classFilePath, $tokens);
         }
     }
+}
 
-    private static function writePublic($level, $token, $isInClass, $i, $tokens, $absolutePath)
-    {
-        if (($level !== 1) || ($token[0] !== T_FUNCTION) || ! $isInClass) {
-            return [$tokens, $i];
-        }
-
-        $t = $i;
-        if (\in_array($tokens[$t - 2][0], [T_STATIC])) {
-            $t = $t - 2;
-        }
-
-        if (! \in_array($tokens[$t - 2][0], [T_PUBLIC, T_PROTECTED, T_PRIVATE])) {
-            array_splice($tokens, $t, 0, [[T_WHITESPACE, ' ']]);
-            array_splice($tokens, $t, 0, [[T_PUBLIC, 'public']]);
-            $i++;
-            $i++;
-            Refactor::saveTokens($absolutePath, $tokens);
-        }
-
+private
+static function writePublic($level, $token, $isInClass, $i, $tokens, $absolutePath)
+{
+    if (($level !== 1) || ($token[0] !== T_FUNCTION) || !$isInClass) {
         return [$tokens, $i];
     }
+
+    $t = $i;
+    if (\in_array($tokens[$t - 2][0], [T_STATIC])) {
+        $t = $t - 2;
+    }
+
+    if (!\in_array($tokens[$t - 2][0], [T_PUBLIC, T_PROTECTED, T_PRIVATE])) {
+        array_splice($tokens, $t, 0, [[T_WHITESPACE, ' ']]);
+        array_splice($tokens, $t, 0, [[T_PUBLIC, 'public']]);
+        $i++;
+        $i++;
+        Refactor::saveTokens($absolutePath, $tokens);
+    }
+
+    return [$tokens, $i];
+}
 }
