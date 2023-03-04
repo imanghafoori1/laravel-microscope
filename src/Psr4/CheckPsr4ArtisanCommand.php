32513e3f03350a71b60da7996a7daa6010c1d7e9
Iman Ghafoori <imanghafoori1@gmail.com>
2023-03-04 20:52:44 +0330

    refactor

--- src/Psr4/CheckPsr4ArtisanCommand.php
+++ src/Psr4/CheckPsr4ArtisanCommand.php
@@ -2,29 +2,23 @@
 
 namespace Imanghafoori\LaravelMicroscope\Psr4;
 
-use Composer\ClassMapGenerator\ClassMapGenerator;
 use Illuminate\Console\Command;
 use Illuminate\Support\Composer;
 use Illuminate\Support\Str;
 use ImanGhafoori\ComposerJson\ComposerJson as Comp;
-use Imanghafoori\Filesystem\Filesystem;
 use Imanghafoori\LaravelMicroscope\Analyzers\ComposerJson;
 use Imanghafoori\LaravelMicroscope\ErrorReporters\ErrorPrinter;
-use Imanghafoori\LaravelMicroscope\FileReaders\FilePath;
-use Imanghafoori\LaravelMicroscope\LaravelPaths\LaravelPaths;
 
 class CheckPsr4ArtisanCommand extends Command
 {
-    private static $pathsForReferenceFix = [];
-
     protected $signature = 'check:psr4
         {--d|detailed : Show classes being checked}
         {--f|force : Fixes namespaces without asking.}
         {--s|nofix : Skips fixing namespaces and only reports them.}
         {--o|force-ref-fix : Fix references without asking.}
         {--r|no-ref-fix : Skips searching for references to fix them.}
-        {--w|watch}
-        {--folder=}';
+        {--w|watch} : Re-runs the command every 8 seconds.
+        {--folder=} : Filter based on partial folder name.';
 
     protected $description = 'Checks the validity of namespaces';
 
@@ -43,7 +37,7 @@ class CheckPsr4ArtisanCommand extends Command
 
         $time = round(microtime(true) - $time, 5);
 
-        $this->handleErrors($errorsLists);
+        HandleErrors::handleErrors($errorsLists, $this);
         $this->printReport($errorPrinter, $time, $composer->readAutoload(), $classLists);
 
         $this->composerDumpIfNeeded($errorPrinter);
@@ -78,101 +72,10 @@ class CheckPsr4ArtisanCommand extends Command
         }
     }
 
-    private function handleError($wrong, $beforeFix, $afterFix)
-    {
-        if ($wrong['type'] === 'namespace') {
-            $absPath = $wrong['absFilePath'];
-            $from = $wrong['currentNamespace'];
-            $to = $wrong['correctNamespace'];
-            $class = $wrong['class'];
-            $relativePath = str_replace(base_path(), '', $absPath);
-
-            CheckPsr4Printer::warnIncorrectNamespace($relativePath, $from, $class);
-
-            if (CheckPsr4Printer::ask($this, $to)) {
-                $this->fix($absPath, $from, $to, $class, $beforeFix, $afterFix, $relativePath);
-            }
-        } elseif ($wrong['type'] === 'filename') {
-            CheckPsr4Printer::wrongFileName($wrong['relativePath'], $wrong['class'], $wrong['fileName']);
-        }
-    }
-
-    private static function getPathForReferenceFix()
-    {
-        if (self::$pathsForReferenceFix) {
-            return self::$pathsForReferenceFix;
-        }
-
-        $paths = [];
-
-        foreach (ComposerJson::readAutoload() as $autoload) {
-            foreach ($autoload as $psr4Path) {
-                foreach (FilePath::getAllPhpFiles($psr4Path) as $file) {
-                    $paths[] = $file->getRealPath();
-                }
-            }
-        }
-
-        $paths = array_merge(ComposerJson::readAutoloadFiles(), $paths);
-
-        foreach (ComposerJson::make()->readAutoloadClassMap() as $classmaps) {
-            foreach ($classmaps as $classmap) {
-                $paths = array_merge($paths, array_values(ClassMapGenerator::createMap($classmap)));
-            }
-        }
-
-        $paths = array_merge($paths, LaravelPaths::collectFilesInNonPsr4Paths());
-
-        self::$pathsForReferenceFix = $paths;
-
-        return $paths;
-    }
-
-    private function afterReferenceFix()
-    {
-        return function ($path, $changedLineNums, $content) {
-            Filesystem::$fileSystem::file_put_contents($path, $content);
-
-            $p = app(ErrorPrinter::class);
-            foreach ($changedLineNums as $line) {
-                $p->simplePendError('', $path, $line, 'ns_replacement', 'Namespace replacement:');
-            }
-        };
-    }
-
-    private function beforeReferenceFix()
-    {
-        if ($this->option('force-ref-fix')) {
-            return function () {
-                return true;
-            };
-        }
-
-        return function ($path, $lineIndex, $lineContent) {
-            $this->getOutput()->writeln(ErrorPrinter::getLink($path, $lineIndex));
-            $this->warn($lineContent);
-            $msg = 'Do you want to update reference to the old namespace?';
-
-            return $this->confirm($msg, true);
-        };
-    }
-
     private function printMessages($messages)
     {
         foreach ($messages as [$message, $level]) {
             $this->$level($message);
-        }
-    }
-
-    private function handleErrors(array $errorsLists)
-    {
-        $before = $this->beforeReferenceFix();
-        $after = $this->afterReferenceFix();
-
-        foreach ($errorsLists as $errors) {
-            foreach ($errors as $wrong) {
-                $this->handleError($wrong, $before, $after);
-            }
         }
     }
 
@@ -206,20 +109,6 @@ class CheckPsr4ArtisanCommand extends Command
         return function ($absFilePath, $fileName) use ($folder) {
             return strpos($absFilePath, $folder);
         };
-    }
-
-    private function fix($absPath, $from, $to, $class, $beforeFix, $afterFix, $relativePath)
-    {
-        NamespaceFixer::fix($absPath, $from, $to);
-
-        if ($from && ! $this->option('no-ref-fix')) {
-            $changes = [
-                $from.'\\'.$class => $to.'\\'.$class,
-            ];
-
-            ClassRefCorrector::fixAllRefs($changes, self::getPathForReferenceFix(), $beforeFix, $afterFix);
-        }
-        CheckPsr4Printer::fixedNamespace($relativePath, $from, $to);
     }
 
     private function getClassLists(Comp $composer)
