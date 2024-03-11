0817214e611a1d7a50165c819393a0e7f3c8f043
Iman Ghafoori <imanghafoori1@gmail.com>
2024-03-12 00:15:46 +0330

    enhance check:blade_queries

--- src/Commands/CheckBladeQueries.php
+++ src/Commands/CheckBladeQueries.php
@@ -6,13 +6,14 @@ use Illuminate\Console\Command;
 use Imanghafoori\LaravelMicroscope\Checks\CheckIsQuery;
 use Imanghafoori\LaravelMicroscope\ErrorReporters\ErrorPrinter;
 use Imanghafoori\LaravelMicroscope\Iterators\BladeFiles;
+use Imanghafoori\LaravelMicroscope\Features\CheckImports\Reporters\BladeReport;
 use Imanghafoori\LaravelMicroscope\Traits\LogsErrors;
 
 class CheckBladeQueries extends Command
 {
     use LogsErrors;
 
-    protected $signature = 'check:blade_queries {--d|detailed : Show files being checked}';
+    protected $signature = 'check:blade_queries {--f|file=} {--d|folder=} {--detailed : Show files being checked}';
 
     protected $description = 'Checks db queries in blade files';
 
@@ -20,20 +21,21 @@ class CheckBladeQueries extends Command
     {
         event('microscope.start.command');
         $this->info('Checking blade files for db queries...');
+        $includeFileName = ltrim($this->option('file'), '=');
+        $includeFolderName = ltrim($this->option('folder'), '=');
 
         $errorPrinter->printer = $this->output;
 
         // checks the blade files for database queries.
-        $this->checkBlades();
+        $bladeStats = BladeFiles::check([CheckIsQuery::class], [], $includeFileName, $includeFolderName);
+
+        $this->getOutput()->writeln(implode(PHP_EOL, [
+            BladeReport::getBladeStats($bladeStats),
+        ]));
 
         $this->finishCommand($errorPrinter);
         $errorPrinter->printTime();
 
         return $errorPrinter->hasErrors() ? 1 : 0;
-    }
-
-    private function checkBlades()
-    {
-        iterator_to_array(BladeFiles::check([CheckIsQuery::class]));
     }
 }
