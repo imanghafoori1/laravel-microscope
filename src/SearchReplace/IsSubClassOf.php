3d6d2271f53181f4ed4640ff586a55df742ffcd7
Iman Ghafoori <imanghafoori1@gmail.com>
2024-03-15 03:19:17 +0330

    bug fix

--- src/SearchReplace/IsSubClassOf.php
+++ src/SearchReplace/IsSubClassOf.php
@@ -2,20 +2,42 @@
 
 namespace Imanghafoori\LaravelMicroscope\SearchReplace;
 
+use Imanghafoori\LaravelMicroscope\Check;
 use Imanghafoori\TokenAnalyzer\GetClassProperties;
 use Imanghafoori\TokenAnalyzer\ParseUseStatement;
 
-class IsSubClassOf
+class IsSubClassOf implements Check
 {
     public static function check($placeholderVal, $parameter, $tokens)
     {
-        if ($placeholderVal[1] === 'self') {
-            [$namespace, $class] = GetClassProperties::readClassDefinition($tokens);
-            $fullClassPath = $namespace.'\\'.$class;
-        } else {
-            $fullClassPath = ParseUseStatement::getExpandedRef($tokens, $placeholderVal[1]);
-        }
+        $fullClassPath = self::getFullClassPath($placeholderVal[1], $tokens);
 
         return is_subclass_of($fullClassPath, $parameter);
+    }
+
+    private static function getFullClassPath($classRef, $tokens): string
+    {
+        if ($classRef === 'self') {
+            [$namespace, $class] = GetClassProperties::readClassDefinition($tokens);
+
+            return $namespace.'\\'.$class;
+        }
+
+        if ($classRef === 'parent') {
+            return self::getParent($tokens);
+        }
+
+        if ($classRef[0] === '\\') {
+            return $classRef;
+        }
+
+        return ParseUseStatement::getExpandedRef($tokens, $classRef);
+    }
+
+    private static function getParent($tokens)
+    {
+        [$namespace, $class, $type] = GetClassProperties::readClassDefinition($tokens);
+
+        return $type === T_CLASS ? get_parent_class($namespace.'\\'.$class) : '';
     }
 }
