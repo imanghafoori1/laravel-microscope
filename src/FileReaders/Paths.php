6317e61c3ce4a93f369f7df9adf8c670e6de579f
Iman <imanghafoori1@gmail.com>
2025-09-25 02:30:16 +0330

    bug fix

--- src/FileReaders/Paths.php
+++ src/FileReaders/Paths.php
@@ -2,7 +2,6 @@
 
 namespace Imanghafoori\LaravelMicroscope\FileReaders;
 
-use Imanghafoori\LaravelMicroscope\Foundations\Loop;
 use Imanghafoori\LaravelMicroscope\Iterators\FiltersFiles;
 use Imanghafoori\LaravelMicroscope\PathFilterDTO;
 
@@ -24,10 +23,13 @@ class Paths
         $fileName = $pathFilter->includeFile;
         is_string($dirs) && ($dirs = [$dirs]);
 
-        return Loop::mapIf(
-            $dirs,
-            fn ($dir) => is_dir($dir),
-            fn ($dir) => self::filterFiles(PhpFinder::getPathsInDir($dir, $fileName), $pathFilter)
-        );
+        $files = [];
+        foreach ($dirs as $dir) {
+            if (is_dir($dir)) {
+                $files[$dir] = self::filterFiles(PhpFinder::getPathsInDir($dir, $fileName), $pathFilter);
+            }
+        }
+
+        return $files;
     }
 }
