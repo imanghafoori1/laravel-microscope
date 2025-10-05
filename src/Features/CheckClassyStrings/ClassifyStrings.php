f6a3ab43fff5751798e5960e22160c0ac9c3d014
Iman <imanghafoori1@gmail.com>
2025-10-06 03:07:57 +0330

    CheckStringyClasseCommand

--- src/Features/CheckClassyStrings/ClassifyStrings.php
+++ src/Features/CheckClassyStrings/ClassifyStrings.php
@@ -2,15 +2,9 @@
 
 namespace Imanghafoori\LaravelMicroscope\Features\CheckClassyStrings;
 
-use Illuminate\Console\Command;
-use Imanghafoori\LaravelMicroscope\ErrorReporters\ErrorPrinter;
-use Imanghafoori\LaravelMicroscope\ErrorReporters\Psr4ReportPrinter;
-use Imanghafoori\LaravelMicroscope\Features\CheckImports\Reporters\Psr4Report;
-use Imanghafoori\LaravelMicroscope\Iterators\CheckSet;
-use Imanghafoori\LaravelMicroscope\Iterators\ForAutoloadedClassMaps;
-use Imanghafoori\LaravelMicroscope\Iterators\ForAutoloadedPsr4Classes;
+use Imanghafoori\LaravelMicroscope\Foundations\BaseCommand;
 
-class ClassifyStrings extends Command
+class ClassifyStrings extends BaseCommand
 {
     protected $signature = 'check:stringy_classes
     {--f|file=}
@@ -21,23 +15,16 @@ class ClassifyStrings extends Command
 
     protected $description = 'Replaces string references with ::class version of them.';
 
-    public function handle(ErrorPrinter $errorPrinter)
-    {
-        $this->info('Checking stringy classes...');
+    public $initialMsg = 'Checking stringy classes...';
 
-        $errorPrinter->printer = $this->output;
+    public $checks = [CheckStringy::class];
+
+    public $customMsg = '';
+
+    public function handleCommand($command)
+    {
         CheckStringy::$command = $this;
 
-        $checkSet = CheckSet::initParams([CheckStringy::class], $this);
-
-        $psr4Stats = ForAutoloadedPsr4Classes::check($checkSet);
-        $classMapStats = ForAutoloadedClassMaps::check($checkSet);
-
-        $lines = Psr4Report::formatAutoloads($psr4Stats, $classMapStats);
-        Psr4ReportPrinter::printAll($lines, $this->getOutput());
-
-        $this->getOutput()->writeln(CheckStringyMsg::finished());
-
-        return $errorPrinter->hasErrors() ? 1 : 0;
+        $command->printAll($command->forComposerLoadedFiles());
     }
 }
