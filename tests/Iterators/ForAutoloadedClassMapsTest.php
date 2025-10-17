1b40c1446e3380edc2cc50e006be78a082013e87
Iman <imanghafoori1@gmail.com>
2025-10-17 17:50:57 +0330

    ForAutoloadedClassMapsTest

--- tests/Iterators/ForAutoloadedClassMapsTest.php
+++ tests/Iterators/ForAutoloadedClassMapsTest.php
@@ -3,7 +3,9 @@
 namespace Imanghafoori\LaravelMicroscope\Tests\Iterators;
 
 use Imanghafoori\LaravelMicroscope\Analyzers\ComposerJson;
+use Imanghafoori\LaravelMicroscope\FileReaders\BasePath;
 use Imanghafoori\LaravelMicroscope\Foundations\Iterator;
+use Imanghafoori\LaravelMicroscope\Foundations\PhpFileDescriptor;
 use Imanghafoori\LaravelMicroscope\Iterators\CheckSet;
 use Imanghafoori\LaravelMicroscope\Iterators\ForAutoloadedFiles;
 use PHPUnit\Framework\TestCase;
@@ -12,31 +14,45 @@ class ForAutoloadedClassMapsTest extends TestCase
 {
     public function setUp(): void
     {
+        $ds = DIRECTORY_SEPARATOR;
+        BasePath::$path = __DIR__;
+        $dir = __DIR__.$ds.'app'.$ds;
+        copy($dir.'MyClass.stub', $dir.'MyClass.php');
+        $_SESSION['file'] = [];
         $_SESSION['msg'] = [];
     }
 
     public function tearDown(): void
     {
+        $ds = DIRECTORY_SEPARATOR;
+
+        BasePath::$path = '';
+        unlink(__DIR__.$ds.'app'.$ds.'MyClass.php');
+
+        unset($_SESSION['file']);
         unset($_SESSION['msg']);
     }
 
-    public function test_basic()
+    public function test_forClassmaps()
     {
         ComposerJson::$composer = function () {
             return new class
             {
                 public function readAutoloadClassMap()
                 {
-                    return ['app'];
+                    return [
+                        '/' => ['app']
+                    ];
                 }
             };
         };
-        $iterator = new Iterator(CheckSet::initParams(new class
+        $iterator = new Iterator(CheckSet::initParams([new class
         {
-            public static function check($file)
+            public static function check(PhpFileDescriptor $file)
             {
+                $_SESSION['file'][] = $file->getFileName();
             }
-        }, new class
+        }], new class
         {
             public function option()
             {
@@ -52,27 +68,31 @@ class ForAutoloadedClassMapsTest extends TestCase
 
         $iterator->printAll($iterator->forClassmaps());
         $this->assertIsArray($_SESSION['msg']);
-        $this->assertCount(3, $_SESSION['msg']);
+        $this->assertEquals('MyClass.php', $_SESSION['file'][0]);
+        $this->assertCount(6, $_SESSION['msg']);
     }
 
-    public function test_basic2()
+    public function test_for_autoloaded_files()
     {
         ComposerJson::$composer = function () {
             return new class
             {
                 public function autoloadedFilesList()
                 {
-                    return ['app.php'];
+                    return [
+                        '/' => ['app/MyClass.php']
+                    ];
                 }
             };
         };
 
-        $checkSet = CheckSet::initParams(new class
+        $checkSet = CheckSet::initParams([new class
         {
-            public static function check($file)
+            public static function check(PhpFileDescriptor $file)
             {
+                $_SESSION['file'][] = $file->getFileName();
             }
-        }, new class
+        }], new class
         {
             public function option()
             {
@@ -90,6 +110,7 @@ class ForAutoloadedClassMapsTest extends TestCase
 
         $iterator->printAll(ForAutoloadedFiles::check($checkSet));
         $this->assertIsArray($_SESSION['msg']);
-        $this->assertCount(4, $_SESSION['msg']);
+        $this->assertCount(5, $_SESSION['msg']);
+        $this->assertEquals(['MyClass.php'], $_SESSION['file']);
     }
 }
