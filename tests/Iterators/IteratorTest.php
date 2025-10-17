cd3acc82aa536831cbb728110b951327762325e7
Iman <imanghafoori1@gmail.com>
2025-10-17 17:49:19 +0330

    IteratorTest.php

--- tests/Iterators/IteratorTest.php
+++ tests/Iterators/IteratorTest.php
@@ -3,14 +3,20 @@
 namespace Imanghafoori\LaravelMicroscope\Tests\Iterators;
 
 use Imanghafoori\LaravelMicroscope\Analyzers\ComposerJson;
+use Imanghafoori\LaravelMicroscope\FileReaders\BasePath;
 use Imanghafoori\LaravelMicroscope\Foundations\Iterator;
+use Imanghafoori\LaravelMicroscope\Foundations\PhpFileDescriptor;
 use Imanghafoori\LaravelMicroscope\Iterators\CheckSet;
+use Imanghafoori\LaravelMicroscope\SpyClasses\RoutePaths;
 use PHPUnit\Framework\TestCase;
 
 class IteratorTest extends TestCase
 {
     public function setUp(): void
     {
+        RoutePaths::$paths = [__DIR__.'/web.php'];
+        BasePath::$path = __DIR__;
+
         $_SESSION['msg'] = [];
     }
 
@@ -30,14 +36,29 @@ class IteratorTest extends TestCase
                         '/' => ['App\\' => 'app'],
                     ];
                 }
+
+                public function readAutoloadClassMap()
+                {
+                    return [
+                        '/' => ['class_map'],
+                    ];
+                }
+
+                public function autoloadedFilesList()
+                {
+                    return [
+                        '/' => ['class_map/MyClass.php'],
+                    ];
+                }
             };
         };
-        $iterator = new Iterator(CheckSet::initParams(new class
+        $iterator = new Iterator(CheckSet::initParams([new class
         {
-            public static function check()
+            public static function check(PhpFileDescriptor $file)
             {
+                $file->getFileName();
             }
-        }, new class
+        }], new class
         {
             public function option()
             {
@@ -56,5 +77,9 @@ class IteratorTest extends TestCase
         $this->assertStringContainsString('<fg=blue>./composer.json</>', $_SESSION['msg'][0]);
         $this->assertStringContainsString('App\\', $_SESSION['msg'][2]);
         $this->assertStringContainsString('./app', $_SESSION['msg'][3]);
+
+        $iterator->formatPrintPsr4Classmap();
+        $iterator->forRoutes();
+        $iterator->formatPrintForComposerLoadedFiles();
     }
 }
