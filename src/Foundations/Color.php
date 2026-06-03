add75187ffa924929dbe2ff1d384383a053b8f27
Iman <imanghafoori1@gmail.com>
2026-06-03 16:46:47 +0330

    add docblocks

--- src/Foundations/Color.php
+++ src/Foundations/Color.php
@@ -8,11 +8,19 @@ class Color
 {
     public static $color = true;
 
+    /**
+     * @param  string  $msg
+     * @return string
+     */
     public static function yellow($msg)
     {
         return self::color($msg, __FUNCTION__);
     }
 
+    /**
+     * @param  string  $msg
+     * @return string
+     */
     public static function boldYellow($msg)
     {
         if (self::$color === false) {
@@ -22,36 +30,65 @@ class Color
         return "<options=bold;fg=yellow>$msg</>";
     }
 
+    /**
+     * @param  string  $msg
+     * @return string
+     */
     public static function red($msg)
     {
         return self::color($msg, __FUNCTION__);
     }
 
+    /**
+     * @param  string  $msg
+     * @return string
+     */
     public static function blue($msg)
     {
         return self::color($msg, __FUNCTION__);
     }
 
+    /**
+     * @param  string  $msg
+     * @return string
+     */
     public static function green($msg)
     {
         return self::color($msg, __FUNCTION__);
     }
 
+    /**
+     * @param  string  $msg
+     * @return string
+     */
     public static function white($msg)
     {
         return self::color($msg, __FUNCTION__);
     }
 
+    /**
+     * @param  string  $msg
+     * @return string
+     */
     public static function gray($msg)
     {
         return self::color($msg, LineSeperator::$color);
     }
 
+    /**
+     * @param  string  $msg
+     * @return string
+     */
     public static function cyan($msg)
     {
         return self::color($msg, __FUNCTION__);
     }
 
+    /**
+     * @param  string  $msg
+     * @param  string  $color
+     * @return string
+     */
     private static function color($msg, $color)
     {
         if (self::$color === false) {
